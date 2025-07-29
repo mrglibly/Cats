2dd43aebe085499506cff6c346ad68183d4f7728
Sergey Boyko <mrglibly@gmail.com>
2025-07-29 17:13:39 +0300

    Update button and Image resize added

--- Cataas.py
+++ Cataas.py
@@ -10,25 +10,32 @@ def load_image(url):
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
+        img.thumbnail((600, 480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"Error occured {e}.")
         return None
 
 
+def set_image():
+    img = load_image(url)
+
+    if img:
+        label.config(image=img)
+        label.image = img
+
 window = Tk()
 window.title('Cats')
-window.geometry('600x480')
+window.geometry('600x520')
 
 label = Label()
 label.pack()
 
+update_button = Button(text='Обновить', command=set_image)
+update_button.pack()
+
 url = 'https://cataas.com/cat'
 
-img = load_image(url)
-
-if img:
-    label.config(image=img)
-    label.image = img
+set_image()
 
 window.mainloop()
