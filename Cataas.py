840c18a674a4ed405399f05609b7291e380aebbe
Sergey Boyko <mrglibly@gmail.com>
2025-07-29 16:54:29 +0300

    Function created to load an image - minor mistakes resolved

--- Cataas.py
+++ Cataas.py
@@ -1,35 +1,34 @@
 from tkinter import *
-from requests import request
+import requests
 from PIL import Image, ImageTk
 from io import BytesIO
 
 
-def load_image():
+def load_image(url):
     try:
-        response = request.get(url)
+        response = requests.get(url)
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
         return ImageTk.PhotoImage(img)
-
     except Exception as e:
-        print(f'Error occured {e}')
+        print(f"Error occured {e}.")
         return None
 
 
 window = Tk()
 window.title('Cats')
-window.geometry('600X480')
+window.geometry('600x480')
 
-label = (Label()
-label.pack())
+label = Label()
+label.pack()
 
 url = 'https://cataas.com/cat'
 
 img = load_image(url)
 
 if img:
-    label.config(image = img)
-    label.imaage - img
+    label.config(image=img)
+    label.image = img
 
 window.mainloop()
