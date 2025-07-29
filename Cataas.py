7b93366d6b50f211cd6c0c1f6112360a0b16b23d
Sergey Boyko <mrglibly@gmail.com>
2025-07-29 16:37:29 +0300

    Image added

--- Cataas.py
+++ Cataas.py
@@ -1,1 +1,21 @@
 from tkinter import *
+from requests import request
+from PIL import Image, ImageTk
+from io import BytesIO
+
+window = Tk()
+window.title('Cats')
+window.geometry('600X480')
+
+label = (Label()
+label.pack())
+
+url = 'https://cataas.com/cat'
+
+img = load_image(url)
+
+if img:
+    label.config(image = img)
+    label.imaage - img
+
+window.mainloop()
