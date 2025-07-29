60f18cb1f6aa3634a923801f202316d368d9b450
Sergey Boyko <mrglibly@gmail.com>
2025-07-29 17:53:23 +0300

    Load By Tag option is added

--- Cataas.py
+++ Cataas.py
@@ -17,11 +17,18 @@ def load_image(url):
         return None
 
 
-def set_image():
-    img = load_image(url)
+def open_new_window():
+    tag = tag_entry.get()
+    url_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
+    img = load_image(url_tag)
 
     if img:
-        label.config(image=img)
+        new_window = Toplevel()
+        new_window.title('Image with a Cat')
+        new_window.geometry('600x480')
+        label = Label(new_window, image=img)
+        label.pack()
+        # label.config(image=img)
         label.image = img
 
 
@@ -30,10 +37,16 @@ def exit():
 
 window = Tk()
 window.title('Cats')
-window.geometry('600x520')
+window.geometry('400x220')
 
 label = Label()
 label.pack()
+
+tag_entry = Entry()
+tag_entry.pack()
+
+load_button = Button(text='Загрузить тэг', command=open_new_window)
+load_button.pack()
 
 # update_button = Button(text='Обновить', command=set_image)
 # update_button.pack()
@@ -45,10 +58,10 @@ window.config(menu=menu_bar)
 
 file_menu = Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label='Файл', menu = file_menu)
-file_menu.add_command(label='Загрузить фото', command=set_image)
+file_menu.add_command(label='Загрузить фото', command=open_new_window)
 file_menu.add_separator()
 file_menu.add_command(label='Выход', command=exit)
 
-set_image()
+open_new_window()
 
 window.mainloop()
