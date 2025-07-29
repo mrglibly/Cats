02b6856ba85ee2d1c2e0192fa73868b64de80ea2
Sergey Boyko <mrglibly@gmail.com>
2025-07-29 16:45:08 +0300

    Function created to load an image

--- Cataas.py
+++ Cataas.py
@@ -3,6 +3,20 @@ from requests import request
 from PIL import Image, ImageTk
 from io import BytesIO
 
+
+def load_image():
+    try:
+        response = request.get(url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+
+    except Exception as e:
+        print(f'Error occured {e}')
+        return None
+
+
 window = Tk()
 window.title('Cats')
 window.geometry('600X480')
