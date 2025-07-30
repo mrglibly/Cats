d6e72a3ccbf68978bc60058861e5d634f1a771fb
Sergey Boyko <mrglibly@gmail.com>
2025-07-30 14:51:55 +0300

    Progress bar added!

--- Dogs.py
+++ Dogs.py
@@ -1,10 +1,21 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+
 def get_dog_image():
+    try:
+        response = requests.get('https://dog.ceo/api/breeds/image/random')
+        response.raise_for_status()
+        data = response.json()
+        return data['message']
+    except Exception as e:
+        messagebox.showerror('Error!', f'Image Load Error occurred: {e}.')
+        return None
+
 
 def show_image():
     img_url = get_dog_image()
@@ -15,24 +26,31 @@ def show_image():
             img_data = BytesIO(response.content)
             img = Image.open(img_data)
             img.thumbnail((300, 300), Image.Resampling.LANCZOS)
+            img = ImageTk.PhotoImage(img)
             label.config(image=img)
             label.image = img
         except Exception as e:
-            print(f"Error occured {e}.")
-            return None
+            messagebox.showerror('Error!', f'The Error occurred {e}.')
+        progress_b.stop()
 
-        return ImageTk.PhotoImage(img)
-
+def prog():
+    progress_b['value'] = 0
+    progress_b.start(30)
+    window.after(3000, show_image)
 
 window = Tk()
 window.title("Dogy Pictures!")
 window.geometry("360x420")
 
-label = Label()
+label = ttk.Label()
 label.pack()
 
-button = Button(text='Загрузить фото', command=show_image)
+button = ttk.Button(text='Загрузить фото', command=prog)
 button.pack(pady=10)
+
+progress_b = ttk.Progressbar(mode='determinate',length=300)
+progress_b.pack(pady=10)
+
 
 # menu_bar = Menu(window)
 # window.config(menu=menu_bar)
